ed548ee7f00c974150e381b6839c28305f2b9db0
xyx <xyx@tensorstack.com>
2023-03-14 17:25:32 +0800

    Fix some problems

--- build-custom-image/README.md
+++ build-custom-image/README.md
@@ -8,7 +8,7 @@
 # cd into current directory
 cd ~/tutorial-examples/build-custom-image
 # create a Secret of docker config, 
-./create-docker-secret.sh <REGISTRY_HOST> <USERNAME> <PASSWORD>
+./create-docker-secret.sh -s docker-config -r <REGISTRY_HOST> -u <USERNAME> -p <PASSWORD>
 # create a WorkflowTemplate
 kubectl apply -f workflow.yaml
 ```
@@ -21,10 +21,10 @@ kubectl create -f workflowrun.yaml
 
 `workflowrun.yaml` 中的各重要参数如下：
 
-* Docker 构建上下文在 PVC 中的路径（位于第 10 行），这里为 `./tutorial-examples/build-custom-image`。
-* dockerfile 在构建上下文中的相对路径（位于第 12 行），这里为 `Dockerfile`。
-* 构建的镜像名称（位于第 14 行）。
-* PVC 名称（位于第 23 行），这里为 `tutorial`。
-* Secret 名称（位于第 26 行），这里为 `docker-config`（在 `create-docker-secret.sh` 中定义）。
+* `contextPath`（9-10 行）：Docker 构建上下文在 PVC 中的路径，这里取 `./tutorial-examples/build-custom-image`。
+* `dockerfile`（11-12 行）：dockerfile 在构建上下文中的相对路径，这里取 `Dockerfile`。
+* `dstImage`（13-14 行）：构建的镜像名称。
+* PVC 名称（23 行）：这里取 `tutorial`。
+* Secret 名称（26 行）：这里取 `docker-config`。
 
 前往工作流控制台查看镜像的构建日志；构建完成的镜像将被推送到相应的 registry 中。
--- build-custom-image/create-docker-secret.sh
+++ build-custom-image/create-docker-secret.sh
@@ -1,6 +1,48 @@
-registry=$1
-username=$2
-password=$3
+#!/bin/bash
+
+# initialize the variables
+secret=""
+registry=""
+username=""
+password=""
+
+# process the named arguments
+while getopts ":s:r:u:p:" opt; do
+  case $opt in
+    s)
+      secret="$OPTARG"
+      ;;
+    r)
+      registry="$OPTARG"
+      ;;
+    u)
+      username="$OPTARG"
+      ;;
+    p)
+      password="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+# check if any arguments are missing
+if [[ -z "$secret" || -z "$registry" || -z "$username" || -z "$password" ]]; then
+  echo "Usage: $0 -s <secret> -r <registry> -u <username> -p <password>" >&2
+  exit 1
+fi
+
+# print the arguments
+echo "Secret: $secret"
+echo "Registry: $registry"
+echo "Username: $username"
+echo "Password: $password"
 
 auth=$(echo -n $username:$password | base64 -w 0)
 configJSON='{"auths":{"'$registry'/v1/":{"username":"'$username'","password":"'$password'","auth":"'$auth'"}}}'
@@ -9,7 +51,7 @@ kubectl apply -f - << EOF
 apiVersion: v1
 kind: Secret
 metadata:
-  name: docker-config
+  name: $secret
 data:
   config.json: $(echo $configJSON | base64 -w 0)
 type: Opaque
